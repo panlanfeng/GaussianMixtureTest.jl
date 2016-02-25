c8c5426265d92125609de898217bb66e7f2900ce
Lanfeng Pan <panlanfeng@gmail.com>
2016-02-24 23:43:44 -0600

    add run100 for C=1

--- examples/run100.jl
+++ examples/run100.jl
@@ -3,6 +3,40 @@ using Distributions
 using KernelEstimator
 using RCall
 
+mu_true = [log(1/0.779 - 1)]
+wi_true = [1.0]
+sigmas_true = [1.2]
+n = 282
+m = MixtureModel(map((u, v) -> Normal(u, v), mu_true, sigmas_true), wi_true)
+
+T1 = zeros(100)
+P = zeros(100)
+for b in 1:100
+    srand(b)
+    x = rand(m, n)
+    T1[b], P[b] = GaussianMixtureTest.kstest(x, 1)
+    print(b,"->", T1[b], " | ")
+end
+
+xs = linspace(0.01, 14, 500)
+den2 = kerneldensity(T1, xeval=xs, kernel=gammakernel, lb=0.)
+
+
+@rput xs den2 T1 P;
+rprint(""" 
+hist(T1, breaks=15, freq=F, ylim=c(0, .4))
+rug(T1)
+lines(xs, dchisq(xs, 2))
+lines(xs, den2, lwd=2, col="blue")
+""")
+
+
+
+import GaussianMixtureTest
+using Distributions
+using KernelEstimator
+using RCall
+
 mu_true = [-2.0858,-1.4879]
 wi_true = [0.0828,0.9172]
 sigmas_true = [0.6735,0.2931]
